52d02c6a5701f0020cc69d3497102a1e566f7c96
Dani <dmontvaid@gmail.com>
2025-10-16 10:58:10 +0200

    Generals created

---
+++ Sprint-1/Automata tesztek/GeneralPage.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from generate_driver import get_preconfigured_chrome_driver
+
+
+class GeneralPage:
+    def __init__(self, url, browser=None):
+        self.URL = url
+        if browser is None:
+            self.browser = get_preconfigured_chrome_driver()
+        else:
+            self.browser = browser
+
+    def get(self):
+        self.browser.get(self.URL)
+
+    def get_current_url(self):
+        return self.browser.current_url
+
+    def close(self):
+        self.browser.close()
+
+    def quit(self):
+        self.browser.quit()

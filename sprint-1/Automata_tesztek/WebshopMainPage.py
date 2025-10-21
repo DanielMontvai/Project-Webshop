8d1c898d3bebb4dce501a696d72bbc8b103c6bb1
Dani <dmontvaid@gmail.com>
2025-10-21 11:36:11 +0200

    back before chat solution

--- sprint-1/Automata_tesztek/WebshopMainPage.py
+++ sprint-1/Automata_tesztek/WebshopMainPage.py
@@ -1,170 +1,104 @@
-# from GeneralPage import GeneralPage
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-#
-# class WebshopMainPage(GeneralPage):
-#     def __init__(self):
-#         self.URL = "http://localhost:4200"
-#         super().__init__(self.URL)
-#
-#     def teardown_method(self):
-#         self.browser.quit()
-#
-#     def price_of_instrument_by_order_number(self, number: int = -1):
-#         wait = WebDriverWait(self.browser, 20)
-#         prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//mat-card-subtitle')))
-#         mp_prices_list = []
-#         if number == -1:
-#             for price in prices:
-#                 mp_prices_list.append(price.text)
-#             return mp_prices_list
-#         else:
-#             return prices[number-1].text
-#
-#
-#     def name_of_instrument(self, number: int = -1):
-#         wait = WebDriverWait(self.browser, 20)
-#         names = wait.until(EC.presence_of_all_elements_located(
-#             (By.XPATH, '//mat-card-title[@class="mat-tooltip-trigger mat-card-title"]')))
-#         mp_names_list = []
-#         if number == -1:
-#             for name in names:
-#                 mp_names_list.append(name.text)
-#             return mp_names_list
-#         else:
-#             return names[number-1].text
-#
-#     def number_of_available_insturments(self):
-#         return self.price_of_instrument_by_order_number()
-#
-#     def name_price_dictionary(self):
-#         instrument_names = self.name_of_instrument()
-#         instrument_prices = self.price_of_instrument_by_order_number()
-#         return dict(zip(instrument_names, instrument_prices))
-#
-#     # def reglogin(self):
-#     #     wait = WebDriverWait(self.browser, 20)
-#     #     wait.until(EC.visibility_of_element_located((By.ID, 'regLogin')))
-#     #     login = wait.until(EC.element_to_be_clickable((By.ID, 'regLogin')))
-#     #     login.click()
-#
-#     def reglogin(self):
-#         wait = WebDriverWait(self.browser, 30)
-#
-#         # click login button
-#         login_button = wait.until(EC.element_to_be_clickable((By.ID, 'regLogin')))
-#         login_button.click()
-#
-#         # ✅ wait for Material overlay to load
-#         wait.until(
-#             EC.presence_of_element_located((By.CSS_SELECTOR, '.cdk-overlay-pane'))
-#         )
-#
-#     def logout(self):
-#         wait = WebDriverWait(self.browser, 20)
-#         wait.until(EC.visibility_of_element_located((By.ID, 'button_logOut')))
-#         button_logout = wait.until(EC.element_to_be_clickable((By.ID, 'button_logOut')))
-#         return button_logout
-#
-#     def input_username(self):
-#         wait = WebDriverWait(self.browser, 20)
-#         wait.until(EC.visibility_of_element_located((By.ID, 'username_input')))
-#         username = wait.until(EC.element_to_be_clickable((By.ID, 'username_input')))
-#         return username
-#
-#     def input_password(self):
-#         wait = WebDriverWait(self.browser, 20)
-#         wait.until(EC.visibility_of_element_located((By.ID, 'password_input')))
-#         password = wait.until(EC.element_to_be_clickable((By.ID, 'password_input')))
-#         return password
-#
-#     def button_login(self):
-#         wait = WebDriverWait(self.browser, 20)
-#         wait.until(EC.visibility_of_element_located((By.XPATH, '//button[@type="submit"]')))
-#         button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
-#
-#         button_login.click()
-#
-#     # def login_process(self, username, passowrd):
-#     #     self.reglogin()
-#     #     self.input_username().send_keys(username)
-#     #     self.input_password().send_keys(passowrd)
-#
-#     def login_process(self, username, password):
-#         self.reglogin()
-#
-#         wait = WebDriverWait(self.browser, 30)
-#
-#         # now inside overlay!
-#         user = wait.until(EC.element_to_be_clickable((By.ID, 'username_input')))
-#         pwd = wait.until(EC.element_to_be_clickable((By.ID, 'password_input')))
-#
-#         user.send_keys(username)
-#         pwd.send_keys(password)
-
 from GeneralPage import GeneralPage
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class WebshopMainPage(GeneralPage):
     def __init__(self):
-        super().__init__("http://localhost:4200")
+        self.URL = "http://localhost:4200"
+        super().__init__(self.URL)
 
-    # ---------------- Prices & Names ----------------
+    def teardown_method(self):
+        self.browser.quit()
+
     def price_of_instrument_by_order_number(self, number: int = -1):
-        wait = WebDriverWait(self.browser, 10)
+        wait = WebDriverWait(self.browser, 20)
         prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//mat-card-subtitle')))
+        mp_prices_list = []
         if number == -1:
-            return [p.text for p in prices]
-        return prices[number-1].text
+            for price in prices:
+                mp_prices_list.append(price.text)
+            return mp_prices_list
+        else:
+            return prices[number-1].text
+
 
     def name_of_instrument(self, number: int = -1):
-        wait = WebDriverWait(self.browser, 10)
+        wait = WebDriverWait(self.browser, 20)
         names = wait.until(EC.presence_of_all_elements_located(
             (By.XPATH, '//mat-card-title[@class="mat-tooltip-trigger mat-card-title"]')))
+        mp_names_list = []
         if number == -1:
-            return [n.text for n in names]
-        return names[number-1].text
+            for name in names:
+                mp_names_list.append(name.text)
+            return mp_names_list
+        else:
+            return names[number-1].text
 
     def number_of_available_instruments(self):
-        return len(self.price_of_instrument_by_order_number())
+        return self.price_of_instrument_by_order_number()
 
     def name_price_dictionary(self):
-        return dict(zip(self.name_of_instrument(), self.price_of_instrument_by_order_number()))
+        instrument_names = self.name_of_instrument()
+        instrument_prices = self.price_of_instrument_by_order_number()
+        return dict(zip(instrument_names, instrument_prices))
 
-    # ---------------- Login ----------------
+    # def reglogin(self):
+    #     wait = WebDriverWait(self.browser, 20)
+    #     wait.until(EC.visibility_of_element_located((By.ID, 'regLogin')))
+    #     login = wait.until(EC.element_to_be_clickable((By.ID, 'regLogin')))
+    #     login.click()
+
     def reglogin(self):
         wait = WebDriverWait(self.browser, 30)
-        login_btn = wait.until(EC.element_to_be_clickable((By.ID, "regLogin")))
-        login_btn.click()
-        # wait for overlay to appear
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".cdk-overlay-pane")))
+
+        # click login button
+        login_button = wait.until(EC.element_to_be_clickable((By.ID, 'regLogin')))
+        login_button.click()
+
+        # ✅ wait for Material overlay to load
+        wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.cdk-overlay-pane'))
+        )
 
     def logout(self):
         wait = WebDriverWait(self.browser, 20)
-        btn_logout = wait.until(EC.element_to_be_clickable((By.ID, "button_logOut")))
-        return btn_logout
+        wait.until(EC.visibility_of_element_located((By.ID, 'button_logOut')))
+        button_logout = wait.until(EC.element_to_be_clickable((By.ID, 'button_logOut')))
+        return button_logout
 
     def input_username(self):
         wait = WebDriverWait(self.browser, 20)
-        return wait.until(EC.element_to_be_clickable((By.ID, "username_input")))
+        wait.until(EC.visibility_of_element_located((By.ID, 'username_input')))
+        username = wait.until(EC.element_to_be_clickable((By.ID, 'username_input')))
+        return username
 
     def input_password(self):
         wait = WebDriverWait(self.browser, 20)
-        return wait.until(EC.element_to_be_clickable((By.ID, "password_input")))
+        wait.until(EC.visibility_of_element_located((By.ID, 'password_input')))
+        password = wait.until(EC.element_to_be_clickable((By.ID, 'password_input')))
+        return password
 
     def button_login(self):
         wait = WebDriverWait(self.browser, 20)
-        btn = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
-        btn.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//button[@type="submit"]')))
+        button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
+
+        button_login.click()
+
+    # def login_process(self, username, passowrd):
+    #     self.reglogin()
+    #     self.input_username().send_keys(username)
+    #     self.input_password().send_keys(passowrd)
 
     def login_process(self, username, password):
         self.reglogin()
-        # overlay input fields
-        user = self.input_username()
-        pwd = self.input_password()
+
+        wait = WebDriverWait(self.browser, 30)
+
+        # now inside overlay!
+        user = wait.until(EC.element_to_be_clickable((By.ID, 'username_input')))
+        pwd = wait.until(EC.element_to_be_clickable((By.ID, 'password_input')))
+
         user.send_keys(username)
         pwd.send_keys(password)
-        self.button_login()
--- sprint-1/Automata_tesztek/test_pricecheck.py
+++ sprint-1/Automata_tesztek/test_pricecheck.py
@@ -1,53 +1,27 @@
-# from WebshopMainPage import WebshopMainPage
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-#
-# class TestWebShop:
-#     def setup_method(self):
-#         self.page = WebshopMainPage()
-#         self.page.get()
-#
-#     def teardown_method(self):
-#         self.page.quit()
-#
-#     def test_pricecheck(self):
-#         assert self.page.price_of_instrument_by_order_number(1) == "Price: $90000"
-#
-#     def test_name(self):
-#         assert self.page.name_of_instrument(1) == "Viola made of birch"
-#
-#     def test_number_available_instruments(self):
-#         assert len(self.page.number_of_available_insturments()) == 22
-#
-#     def test_login(self):
-#         self.page.login_process('forlogin', 'Forlogin@1')
-#         self.page.button_login()
-#         self.page.logout()
-#         assert self.page.logout().is_displayed()
-
-import pytest
 from WebshopMainPage import WebshopMainPage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class TestWebShop:
-
-    @pytest.fixture(scope="class", autouse=True)
-    def setup_class(self):
-        # Initialize page object
+    def setup_method(self):
         self.page = WebshopMainPage()
-        self.page.get()  # open main page
-        yield
-        # Teardown
+        self.page.get()
+
+    def teardown_method(self):
         self.page.quit()
 
+    def test_pricecheck(self):
+        assert self.page.price_of_instrument_by_order_number(1) == "Price: $90000"
+
+    def test_name(self):
+        assert self.page.name_of_instrument(1) == "Viola made of birch"
+
+    def test_number_available_instruments(self):
+        assert len(self.page.number_of_available_instruments()) == 22
+
     def test_login(self):
-        """Test that a user can login successfully"""
-        username = "forlogin"
-        password = "Forlogin@1"
-
-        # Use your page methods
-        self.page.login_process(username, password)
-
-        # Wait for logout button to appear as indication of successful login
-        logout_btn = self.page.logout()
-        assert logout_btn.is_displayed(), "Logout button should be visible after login"
+        self.page.login_process('forlogin', 'Forlogin@1')
+        self.page.button_login()
+        self.page.logout()
+        assert self.page.logout().is_displayed()

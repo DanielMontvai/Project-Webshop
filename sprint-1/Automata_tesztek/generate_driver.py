f3f1e0a41a9f3e8fc8feaa4cea049175a2281f23
Dani <dmontvaid@gmail.com>
2025-10-22 01:01:47 +0200

    waits gd correction

--- sprint-1/Automata_tesztek/generate_driver.py
+++ sprint-1/Automata_tesztek/generate_driver.py
@@ -13,6 +13,8 @@ def get_preconfigured_chrome_driver():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--single-process")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--remote-allow-origins=*")
         service = Service("/usr/bin/chromedriver")

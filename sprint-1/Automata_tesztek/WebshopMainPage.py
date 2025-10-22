b0864b224ba94b529e508872f0667b814dfff581
Dani <dmontvaid@gmail.com>
2025-10-22 20:56:14 +0200

    elementclickinterceptedexception imported

--- sprint-1/Automata_tesztek/WebshopMainPage.py
+++ sprint-1/Automata_tesztek/WebshopMainPage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
 class WebshopMainPage(GeneralPage):
